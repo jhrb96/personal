4c151a695183e562b5582ce9a9424b19d8c0872b
jhrb96 <jrodriguezbadell@gmail.com>
2023-12-02 13:19:49 +0000

    Update Plotting_Graph.py

--- Finance/Plotting_Graph.py
+++ Finance/Plotting_Graph.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
+import random
+
 
 squares = [1, 4, 9, 16, 25]
+wildcard = random.randint(27, 50)
+
+squares.append(wildcard)
+
+print(wildcard)
 
 fig, ax = plt.subplots()
 ax.plot(squares)
