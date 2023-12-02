1570bc0303f48b9dd5150f3b396d75a2f4fc6187
jhrb96 <jrodriguezbadell@gmail.com>
2023-12-02 15:07:08 +0000

    plotting cumulative returns

---
+++ Finance/finance youtube.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import datetime as dt
+import yfinance as yf
+
+# Basic Example
+stock = input()
+
+# Date range for today
+end_date = dt.datetime.now()
+start_date = end_date - dt.timedelta(days=365 * 5)
+
+df = yf.download(stock, start=start_date, end=end_date)
+
+df.head()
+
+adj_close_prices = df['Adj Close']
+
+log_returns = np.log(adj_close_prices/adj_close_prices.shift(1))
+
+cumulative_log_returns = log_returns.cumsum()
+
+cumulative_log_returns.plot(title="Cumulative Returns", figsize=(10, 6))
